a9dd03cf718eb3015667a05e150f15dd6eba0bc2
Rashi Abramson <roshkins@gmail.com>
2017-04-11 14:39:13 -0700

    basic dom layout created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,20 +6,27 @@ class App extends Component {
     return (
       <div className="App">
         <div className="SubredditList">
+          <h1>Filter Subreddits</h1>
           <ul>
-            {/* List of r/Popular subreddits */}
-            <li>r/AskReddit</li>
-            <li>r/gadgets</li>
-            <li>r/sports</li>
-            <li>r/pics</li>
-            <li>r/worldnews</li>
-            <li>r/gaming</li>
+            <li className="Subreddit">r/AskReddit</li>
+            <li className="Subreddit">r/gadgets</li>
+            <li className="Subreddit">r/sports</li>
+            <li className="Subreddit">r/pics</li>
+            <li className="Subreddit">r/worldnews</li>
+            <li className="Subreddit">r/gaming</li>
           </ul>
+          <form>
+            <input type="text" placeholder="r/subreddit" />
+          </form>
+
           </div>
           <ol className="Posts">
             <li className="Post">
               <div className="Score">17.1k</div>
               <img alt="thumbnail" className="thumbnail" src="//b.thumbs.redditmedia.com/AwdUKfpVjz_1T7JMPfePIrS34IgEbtvEy_dWlr3bHmE.jpg"></img>
+              <div className="SubmissionTime">submitted 4 hours ago</div>
+              <div className="Author">by Idkmtbh</div>
+              <div className="Subreddit">to r/mildlyinteresting</div>
             </li>
           </ol>
       </div>
