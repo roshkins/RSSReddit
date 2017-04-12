1198ed9a65fbdf69e90043b28d9312306d5dbc58
Rashi Abramson <roshkins@gmail.com>
2017-04-12 14:11:14 -0700

    Added snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('survives refactoring without changing ui', () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
